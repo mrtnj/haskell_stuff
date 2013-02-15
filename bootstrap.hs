e6f11321007049a8a909af89354dbd6e6190624f
mrtnj <sorill@gmail.com>
2013-02-15 23:58:41 +0100

    bootstrap mean of one sample with 2.5%-97.5% a quantile interval

--- bootstrap.hs
+++ bootstrap.hs
@@ -8,17 +8,17 @@
 
 import System.Random
 import Data.List.Split
+import Data.List
 
 -- Input and seeding of the rng will be done with some monad later
-sample = [10,20,30]
-randomNumbers = take 1000 (randomRs (0, (length sample - 1)) (mkStdGen 42))
+-- sample = [10,20,30]
 
 
 -- Make bootstrap replicates in two steps: make random draws of the data
 -- (with replacement), then apply them to the data to get bootstrap
 -- replicates.
 
-generateBootstrap originalData nBootstraps =
+generateBootstrap originalData nBootstraps randomNumbers =
     applyShuffles originalData
         (generateShuffles (length originalData) randomNumbers nBootstraps)
 
@@ -38,7 +38,7 @@ applyShuffles x shuffles =
     
     
 -- Functions for parsing lines into a sample
-someData = ["1,10 ", "1,20", "1,30"]
+-- someData = ["1,10 ", "1,20", "1,30"]
 
 parseSample inData =
     map parseLine (splitOn "\n" inData)
@@ -55,17 +55,43 @@ castNumbers xs =
 separateSamples =
     map snd
 
-        
+
+    
+-- Functions to calculate statistics on bootstrap replicates
+
+mean x = realToFrac (sum x) / genericLength x
+
+bootstrapMeans =
+    map mean
+
+-- Rough quantile function. (Note: this is probably not the best estimate
+-- of the quantile.)
+quantile q xs =
+    sort xs !! (round (q * (genericLength xs - 1)))
+
+bootstrapQuantiles xs =
+    (quantile 0.025 xs, quantile 0.975 xs)
+    
+
 
 -- Quite useless main function
 main = do
     print "boot"
     print "will eventually bootstrap, if martin knows his stuff"
-    inData <- readFile "testdata.txt"
-    --let lines = parseFile inData
+    fileName <- getLine
+    inData <- readFile fileName
     let aSample = splitSample (parseSample inData)
     let a = separateSamples aSample
-    let boot = generateBootstrap a 10
-    print boot
+    print a
+    generator <- getStdGen
+    let randomNumbers = take (100 * length a) (randomRs (0, (length a - 1)) generator)
+    let boot = generateBootstrap a 100 randomNumbers
+    let means = bootstrapMeans boot
+    let estimate = mean means
+    let quantiles = bootstrapQuantiles means
+    --print boot
+    --print means
+    print estimate
+    print quantiles
     
     
