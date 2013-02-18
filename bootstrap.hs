ce4ff405d9d28159649c860a98f7aeefab46e8fa
mrtnj <sorill@gmail.com>
2013-02-18 23:01:44 +0100

    type declarations

--- bootstrap.hs
+++ bootstrap.hs
@@ -16,6 +16,7 @@ import Data.List
 -- Make bootstrap replicates in two steps: make random draws of the data
 -- (with replacement), then apply them to the data to get bootstrap
 -- replicates.
+generateBootstrap :: [Float] -> Int -> [Int] -> [[Float]]
 generateBootstrap originalData nBootstraps randomNumbers =
     applyShuffles originalData
         (generateShuffles (length originalData) randomNumbers nBootstraps)
@@ -23,6 +24,7 @@ generateBootstrap originalData nBootstraps randomNumbers =
 
 -- Take the appropriate number of random draws to generate the shuffle
 -- for a single bootstrap replicate.
+generateShuffles :: Int -> [Int] -> Int -> [[Int]]
 generateShuffles nData randomNumbers 0 = []
 generateShuffles nData randomNumbers nBootstraps = 
     [take nData randomNumbers] ++
@@ -30,6 +32,7 @@ generateShuffles nData randomNumbers nBootstraps =
     
     
 -- Apply shuffle to a single bootstrap replicate.
+applyShuffles :: [Float] -> [[Int]] -> [[Float]]
 applyShuffles x shuffles =
     map applyShuffle shuffles
         where applyShuffle  = map (x !!)
@@ -38,45 +41,53 @@ applyShuffles x shuffles =
     
     
 -- Functions for parsing lines into a sample
-
+parseSample :: String -> [Float]
 parseSample = separateSamples . formatSample . parseFile
 
 -- Parse a file of data.
+parseFile :: String -> [[Float]]
 parseFile inFile =
     map parseLine (splitOn "\n" inFile)
         where parseLine = castNumbers . splitOn "," . filter (/= ' ')
 
 -- Read lists of strings to Float
+castNumbers :: [String] -> [Float]
 castNumbers xs =
     map readNumber xs
         where readNumber x = read x::Float
         
 -- Split lists of data into tuples of (indicator, sample value)
+formatSample :: [[a]] -> [(a,a)]
 formatSample inData =
     map f inData
         where f x = (x !! 0, x !! 1)
 
 -- Take the sample values out of tuples
+separateSamples :: [(a, a)] -> [a]
 separateSamples =
     map snd
-
-
+    
+    
     
 -- Functions to calculate statistics on bootstrap replicates
 
 -- Mean of a list
+mean :: [Float] -> Float
 mean x = realToFrac (sum x) / genericLength x
 
+bootstrapMeans :: [[Float]] -> [Float]
 -- Apply means to bootstrap replicates
 bootstrapMeans =
     map mean
 
 -- Rough quantile function. (Note: this is probably not the best estimate
 -- of the quantile.)
+quantile :: Float -> [Float] -> Float
 quantile q xs =
     sort xs !! (round (q * (genericLength xs - 1)))
 
 -- Pull out (0.025 and 0.975) quantiles
+bootstrapQuantiles :: [Float] -> (Float, Float)
 bootstrapQuantiles xs =
     (quantile 0.025 xs, quantile 0.975 xs)
     
